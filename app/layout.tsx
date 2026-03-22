d7b911c1ab2f25842ebb13fb8f1a9275c51c8e61
Caarlos3 <crt.981995@gmail.com>
2026-03-22 20:29:06 +0100

    feat/mejora de portfolio para Rafa

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,12 +14,12 @@ const vercettiFont = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Mohit Virli ✌️",
+  title: "Rafael Ortiz Larios ✌️",
   description: "A frontend developer by profession, a creative at heart.",
-  keywords: "Mohit Virli, Frontend Engineer, React Developer, Three.js, Creative Developer, Web Development, Angular, JavaScript, TypeScript, Portfolio",
-  authors: [{ name: "Mohit Virli" }],
-  creator: "Mohit Virli",
-  publisher: "Mohit Virli",
+  keywords: "Rafael Ortiz Larios, Frontend Engineer, React Developer, Three.js, Creative Developer, Web Development, Angular, JavaScript, TypeScript, Portfolio",
+  authors: [{ name: "Rafael Ortiz Larios" }],
+  creator: "Rafael Ortiz Larios",
+  publisher: "Rafael Ortiz Larios",
   formatDetection: {
     email: false,
     address: false,
@@ -36,16 +36,16 @@ export const metadata: Metadata = {
     },
   },
   openGraph: {
-    title: "Mohit Virli - Frontend Engineer",
+    title: "Rafael Ortiz Larios - Frontend Engineer",
     description: "Frontend engineer by profession, creative at heart.",
-    url: "https://mohitvirli.github.io",
-    siteName: "Mohit Virli's Portfolio",
-    locale: "en_US",
+    url: "https://rafaelortizlarios.github.io",
+    siteName: "Rafael Ortiz Larios's Portfolio",
+    locale: "es_ES",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Mohit Virli - Frontend Engineer",
+    title: "Rafael Ortiz Larios - Frontend Engineer",
     description: "Frontend engineer by profession, creative at heart.",
   },
   verification: {
